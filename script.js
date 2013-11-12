09d39fc11a7efcf5950307e1fa49c0aca662e1e1
Adrian Westlund <adrianwestlund@hotmail.com>
2013-11-12 14:00:19 +0100

    HEj

--- script.js
+++ script.js
@@ -4,26 +4,46 @@ function get_query(){
 	    var nr = url.substring(url.indexOf('?')).split('=');
 	    if(isNaN(nr[1]))
 	    	nr[1] = 0;
+	    //console.log(nr[0]);
 	    return nr[1];
 }
 
-$(document).ready(function(){
-	var tall = get_query();
-	console.log(tall);
-	var back = tall - 1;
-	var forward = ++tall;
-	console.log(tall);
-	if(back > 0)
-	{
-		$("section").append("<nav id='secNav'><ul><li><a href='index.php?page="+back+"'><span class='hideInCss'>Previous page</span><div id='arrowL'></div></a></li><li><a href='index.php?page="+forward+"'><span class='hideInCss'>Next page</span><div id='arrowR'></div></a></li></ul></nav>");
-	}else
-	{
-		if(back < 0)
-		{
-			forward++;
-		}
-		$("section").append("<nav id='secNav'><ul><li><a href='index.php?page="+forward+"'><span class='hideInCss'>Next page</span><div id='arrowR'></div></a></li></ul></nav>");
+jQuery(function(){
+var trigger_keys = [37,39];
+var left = 0;
+var right = 1;
+$(document).keydown(function(e){
+    if(e.keyCode === trigger_keys[left]){
+        $("#arrowL").click();
 	}
-
+    if(e.keyCode === trigger_keys[right]){
+        $("#arrowR").click();
+    }
+});
 });
 
+
+$(document).ready(function(){
+	var tall = get_query();
+	var max = 10;
+	//console.log(tall);
+	var back = tall - 1;
+	var forward = ++tall;
+	//console.log(tall);
+
+	$("section").append("<nav id='secNav'><ul id='ulSec'></ul></nav>");
+	if(back > 0)
+	{
+		$("#ulSec").append("<li><a href='index.php?page="+back+"'><span class='hideInCss'>Previous page</span><div id='arrowL'></div></a></li>");
+	}
+	if(forward > 1 && forward < max)
+	{
+		$("#ulSec").append("<li><a href='index.php?page="+forward+"'><span class='hideInCss'>Next page</span><div id='arrowR'></div></a></li>");
+	}
+});
+$("#knapp").click(function()
+			{
+				hide=document.getElementsByClassName("hide");
+
+				$( ".hide" ).toggle(600)
+			});
